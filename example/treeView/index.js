064e595f313162dc757d1775a35108e6c73556bb
sanshi <zhz961101@gmail.com>
2018-12-20 22:59:11 +0800

    TreeView demo

---
+++ example/treeView/index.js
@@ -0,0 +1,56 @@
+const $ = q => document.querySelector.call(document,q)
+Poi.prototype.mod("node",$("#nodetpl").innerHTML)
+
+let data = {
+    name: 'My Tree',
+    opened: true,
+    children: [{
+            name: 'hello',
+        },
+        {
+            name: 'wat',
+        },
+        {
+            name: 'child folder',
+            opened: true,
+            children: [{
+                    name: 'child folder',
+                    opened: true,
+                    children: [{
+                            name: 'hello',
+                        },
+                        {
+                            name: 'wat',
+                        }
+                    ]
+                },
+                {
+                    name: 'hello',
+                },
+                {
+                    name: 'wat',
+                },
+                {
+                    name: 'child folder',
+                    opened: false,
+                    children: [{
+                            name: 'hello',
+                        },
+                        {
+                            name: 'wat',
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+const app = new Poi({
+    el: "#app",
+    tpl: "<ul>{{$ node(TreeData)}}</ul>",
+    data:{
+        remit:1,
+        TreeData: data
+    }
+})
