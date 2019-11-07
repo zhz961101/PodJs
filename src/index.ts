88b22a29fc4aa9ef54c0f9735c6bcaeae91cce8e
sanshi <zhz961101@gmail.com>
2019-11-08 01:28:43 +0800

    feat: modify api, add Use function.

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,41 @@ export { ViewModel, __Global__ } from "./mvvm/mvvm";
 export { h, HTMLExtender } from "./tools/html";
 export { defineStatement } from "./compiler/statement";
 export { VFragment } from "./vdom/frag";
+
+import { difineDirective } from "./compiler/directives";
+import { effect, reactive, computed } from "./reactivity/reactivity";
+import { createElement, render } from "./vdom/vdom";
+import { Store } from "./store/store";
+import { createApp } from "./taco/taco";
+import { ViewModel, __Global__ } from "./mvvm/mvvm";
+import { h, HTMLExtender } from "./tools/html";
+import { defineStatement } from "./compiler/statement";
+import { VFragment } from "./vdom/frag";
+
+const TacoExports = {
+    defineStatement,
+    difineDirective,
+    effect,
+    reactive,
+    computed,
+    createElement,
+    render,
+    Store,
+    createApp,
+    ViewModel,
+    __Global__,
+    h,
+    HTMLExtender,
+    VFragment,
+    Use
+}
+
+export default TacoExports;
+
+export interface TacoPlug {
+    install(taco: any): void
+}
+
+export function Use(plug: TacoPlug) {
+    plug.install(TacoExports)
+}
