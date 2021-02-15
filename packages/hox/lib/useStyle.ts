81c3d7c0fea8c26d1c38493d9ca78013ae427b47
sanshi <zhz961101@gmail.com>
2021-02-15 17:49:21 +0800

    :sparkles: cached useStyle.

--- packages/hox/lib/useStyle.ts
+++ packages/hox/lib/useStyle.ts
@@ -38,31 +38,79 @@ const style2css = (style: StyleProperties, supperSelector: string) => {
     return styleText;
 };
 
+const styleRegistry = new (class StyleRegistry {
+    idx2Style = new Map<string, StyleProperties>();
+    style2idx = new Map<string, string>();
+    idxCount = new Map<string, number>();
+
+    hash(sp: StyleProperties) {
+        return encodeURIComponent(JSON.stringify(sp));
+    }
+
+    has(sp: StyleProperties) {
+        return this.idx2Style.has(this.hash(sp));
+    }
+
+    register(sp: StyleProperties) {
+        const hash = this.hash(sp);
+        if (this.has(sp)) {
+            const idx = this.style2idx.get(hash);
+            this.idxCount.set(idx, (this.idxCount.get(idx) || 0) + 1);
+            return this.style2idx.get(hash)!;
+        }
+        const idx = Math.random().toString(36).substr(2);
+        this.idx2Style.set(idx, sp);
+        this.style2idx.set(hash, idx);
+        this.idxCount.set(idx, 1);
+        return idx;
+    }
+
+    unregister(idx: string) {
+        if (!this.idx2Style.has(idx)) {
+            return;
+        }
+        this.idxCount.set(idx, (this.idxCount.get(idx) || 0) - 1);
+        if (this.idxCount.get(idx) > 0) {
+            return;
+        }
+        const sp = this.idx2Style.get(idx);
+        const hash = this.hash(sp);
+        this.style2idx.delete(hash);
+        this.idx2Style.delete(idx);
+        this.idxCount.delete(idx);
+    }
+})();
+
 export const useStyle = (
     styleOrFactory: Mptr<StyleProperties> | (() => StyleProperties),
-    selector = (idx: string) => `[data-style-idx=${idx}]`,
+    selectorFn = (idx: string) => `[data-style-idx=${idx}]`,
     bindStyleFn = (elem: HTMLElement, idx: string) =>
         elem.setAttribute('data-style-idx', idx),
 ) => {
-    const styleIdx = useRef(() => Math.random().toString(36).substr(2));
-    const styleSelector = useMemo(() => selector(styleIdx.value));
-
+    const styleIdx = useRef('');
     const styleContainer = useRef(document.createElement('style'));
-    useEffect(() => {
-        (document.head || document.body || document.children[0]).appendChild(
-            styleContainer.value,
-        );
-    });
     useWatch(
         () =>
             typeof styleOrFactory === 'function'
                 ? styleOrFactory()
                 : unref(styleOrFactory),
-        (currentStyle: StyleProperties) =>
-            (styleContainer.value.innerHTML = style2css(
+        (currentStyle: StyleProperties) => {
+            const idx = styleRegistry.register(currentStyle);
+            styleIdx.value = idx;
+
+            const container =
+                document.head || document.body || document.children[0];
+            if (!container.contains(styleContainer.value)) {
+                container.appendChild(styleContainer.value);
+            }
+
+            styleContainer.value.innerHTML = style2css(
                 currentStyle,
-                styleSelector.value,
-            )),
+                selectorFn(idx),
+            );
+
+            return () => styleRegistry.unregister(idx);
+        },
     );
 
     return (elemRef: HTMLElement) => bindStyleFn(elemRef, unref(styleIdx));
